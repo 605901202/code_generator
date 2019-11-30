b621bc9ec72f48a95ed6f48f246e9b93845533b0
ZhangHao <605901202@qq.com>
2019-11-30 23:15:49 +0800

    2019年4月28日

--- src/main/java/io/renren/CodeGeneratorApplication.java
+++ src/main/java/io/renren/CodeGeneratorApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ZhangHao
+ */
 @SpringBootApplication
 @MapperScan("io.renren.dao")
-public class RenrenApplication {
+public class CodeGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenrenApplication.class, args);
+		SpringApplication.run(CodeGeneratorApplication.class, args);
 	}
 }
